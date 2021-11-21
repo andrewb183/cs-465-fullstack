fe4e4843a9fa60e48f870f5e098c528397343fb5
andrewb183 <andy.r.bltn@gmail.com>
2021-11-21 18:30:58 -0500

    json

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 const travel = (req,res) => {
     pagetitle = process.env.npm_package_description + '- Travel ';
-    res.render('travel', {title: 'Travlr Geta aways'});
+    res.render('travel', {title: 'Travlr Geta aways',trips});
 };
 
 module.exports = {
