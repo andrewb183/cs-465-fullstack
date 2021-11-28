ce6614190d6e8d6ee6be2a37b6923c836b24c80f
andrewb183 <andy.r.bltn@gmail.com>
2021-11-28 18:48:26 -0500

    APIpart1

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    modelBaseDirectory: 'app_server/model', // model directory name
+    modelBaseDirectory: 'app_api/model', // model directory name
     models: ['*.js','db.js'], // model matcher
     data: 'data', // data directory name
     db: 'mongodb://localhost:27017/travlr'
---
+++ app_api/controllers/trips.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const modle = mongoose.model('trips');
+
+// get trips -listing all trips out their
+const tripsList = async(req, res) =>{
+    modle
+        .find({}) // empty filter for all
+        .exec((err,trips) => {
+            if (!trips){
+                return res
+                .status(404)
+                .json({"message":"trip not found"});
+            } else if (err){
+            return res
+            .status(404)
+            .json(err);
+            } else {
+                return res
+                .status(200)
+                .json(trips);
+            }
+
+        });
+}
+
+const tripsFindCode = async(req, res) =>{
+    modle
+        .find({'code':req.params.tripCode}) // empty filter for all
+        .exec((err,trips) => {
+            if (!trips){
+                return res
+                .status(404)
+                .json({"message":"trip not found"});
+            } else if (err){
+            return res
+            .status(404)
+            .json(err);
+            } else {
+                return res
+                .status(200)
+                .json(trips);
+            }
+
+        });
+}
+
+modle.exports = {
+    tripsList,
+    tripsFindCode
+};
