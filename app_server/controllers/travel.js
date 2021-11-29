e33606037e4b2992eda8234bacde9a0ac78d7181
andrewb183 <andy.r.bltn@gmail.com>
2021-11-28 19:42:47 -0500

    APIpart2

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,47 @@
-const fs = require('fs');
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
-const travel = (req,res) => {
-    pagetitle = process.env.npm_package_description + '- Travel ';
-    res.render('travel', {title: 'Travlr Geta aways',trips});
-};
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
+
+  
+ const renderTravelList = (res, responseBody) => {
+     let message = null;
+     let pageTitle = process.env.npm_package_description + ' Travel';
+     if(!(responseBody instanceof Array)){
+         message = 'API lookup error';
+         response = [];
+     } else {
+         if (!responseBody.length){
+             message = 'NO trips exist in databasesel';
+         }
+     }
+     res.render('travel',{
+         title: pageTitle,
+         trips: responsBody,
+         message
+     });
+ }
+
+/* get travle list*/
+const travelList = (req,res) => {
+   const path ='/api/trips';
+   const requestOptions ={
+       url: '${apiOptions.server}${path}',
+       method: 'GET',
+       json: {},
+   };
+   console.info('>>travelcontroller.travelList calling' + requestOptions.url);
+   request(
+       apiOptions,
+       (err, {statusCode}, body) => {
+           if (err) {
+               console.error(err);
+           }
+           renderTravelist(req,res,body);
+       }
+    }
+}
 
 module.exports = {
-    travel
-}
+    travelList
+};
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const controllers = require('../controllers/travel');
 /* GET home page. */
-router.get('/', controllers.travel);
+router.get('/', controllers.travelList);
 
 module.exports = router;
