3a03610ab67f8730d62f75ba48b17de4f0cc8065
andrewb183 <75588107+andrewb183@users.noreply.github.com>
2022-02-13 23:40:48 -0500

    hi

---
+++ app_server/routes/travel.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const controllers = require('../controllers/travel');
+/* GET home page. */
+router.get('/', controllers.travelList);
+
+module.exports = router;
